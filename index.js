c7068f974f2ee084fcffe4c8eeb451d9363218f8
Obaidul1545 <mdobaidulislam128@gmail.com>
2025-12-07 22:30:26 +0600

    user get api created

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
     const usersCollection = db.collection('users');
 
     // users releted apis
+    app.get('/users', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        return res.status(400).send({ error: 'Email missing' });
+      }
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       user.accountStatus = 'pending';
