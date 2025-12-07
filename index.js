0c7f94f50825be73362311bb166cf94532811584
Obaidul1545 <mdobaidulislam128@gmail.com>
2025-12-07 23:25:33 +0600

    firebase token varify

--- index.js
+++ index.js
@@ -4,10 +4,37 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+const admin = require('firebase-admin');
+
+// const serviceAccount = require('./garments-tracker-firebase-adminsdk.json');
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
+  'utf8'
+);
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers?.authorization;
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  try {
+    const idToken = token.split(' ')[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    req.decoded_email = decoded.email;
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+};
 
 const uri = process.env.MONGO_URI;
 
